33aa29eddc823f85827e64768104fd5eba906357
Juan Sánchez <JuanchoSL@hotmail.com>
2025-02-28 19:34:06 +0100

    cleaning code

--- README.md
+++ README.md
@@ -2,37 +2,60 @@
 
 ## Description
 
-This library groups the definitions of the different elements involved in data transmissions such as HTTP, applying the interfaces defined in PSRS 7 and 17, adding the extra objects used and ensuring compliance with the requirements that allow to give the necessary stability To our application, being able to implement for third -party bookstores without the need to adapt our business logic to a new implementation that could modify its consumption or internal structure.
+This library implements the definitions of the different elements involved in data transmissions such as HTTP, applying the interfaces defineds in PSRS 7 and 17, adding the extra objects used and ensuring compliance with the requirements that allow to give the necessary stability to our application, being able to implement for third -party bookstores without the need to adapt our business logic to a new implementation that could modify its consumption or internal structure.
 
 ## Install
+
 ```bash
 composer require juanchosl/httpdata
-composer update
 ```
 
 ## Contents
 
-The Containers folders, includes the distinct elements needed for a data transmission. You can view his definitions for each required block on the official page
+Includes the needed implementations for a data transmission. You can view his definitions for each required block on the official page
 
 ### PSR-7 Messages
 
-Implementation of elements included into [PSR-7](https://www.php-fig.org/psr/psr-7)
+The Containers folder/namespace, implementation of elements included into [PSR-7](https://www.php-fig.org/psr/psr-7)
 
-* Message base class
-* Request (extends Message, Client side)
-* ServerRequest (extends Request, Server side)
-* Response (extends Message)
-* Streams
-* Uris
-* Uploaded files
+- Message base class
+- Request (extends Message, Client side)
+- ServerRequest (extends Request, Server side)
+- Response (extends Message)
+- Streams
+- Uris
+- Uploaded files
 
 ### PSR-17 Factories
 
-Defines how create the objects, ensuring the full compatibility between libraries, applying the [PSR-17](https://www.php-fig.org/psr/psr-17)
+The Factories folder/namespace, defines how create the objects, ensuring the full compatibility between libraries, applying the [PSR-17](https://www.php-fig.org/psr/psr-17)
 
-* Server request
-* Client request
-* Response
-* Streams
-* Uris
-* Uploaded files
+- Server request
+- Client request
+- Response
+- Streams
+- Uris
+- Uploaded files
+
+### FIG Message utils
+
+The library includes the messages utils from FIGs of PHP, includes the
+
+#### RequestMethodsInterface
+
+Constants for the distincts http available methods
+
+- GET
+- POST
+- PUT
+- PATCH
+- DELETE
+- HEAD
+- OPTIONS
+- PURGE
+- TRACE
+- CONNECT
+
+#### StatusCodesInterface
+
+All the standard status codes, for use across your system
--- src/Factories/ServerRequestFactory.php
+++ src/Factories/ServerRequestFactory.php
@@ -2,13 +2,8 @@
 
 namespace JuanchoSL\HttpData\Factories;
 
-use JuanchoSL\DataTransfer\Enums\Format;
-use JuanchoSL\DataTransfer\Factories\DataTransferFactory;
-use JuanchoSL\Exceptions\UnsupportedMediaTypeException;
 use JuanchoSL\HttpData\Factories\UriFactory;
 use JuanchoSL\HttpData\Containers\ServerRequest;
-use JuanchoSL\HttpHeaders\Constants\Types\MimeTypes;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
@@ -41,51 +36,11 @@ class ServerRequestFactory implements ServerRequestFactoryInterface
             }
             if (in_array('application/x-www-form-urlencoded', $content_types) || in_array('multipart/form-data', $content_types)) {
                 $body = $_POST;
-            } else {
-                foreach ($content_types as $content_type) {
-                    switch ($content_type) {
-                        case MimeTypes::JSON:
-                            $type = Format::JSON;
-                            break;
-                        case MimeTypes::CSV:
-                            $type = Format::CSV;
-                            break;
-                        case MimeTypes::XML:
-                            $type = Format::XML;
-                            break;
-                        case MimeTypes::EXCEL:
-                            $type = Format::EXCEL_XLSX;
-                            break;
-                        default:
-                            throw new UnsupportedMediaTypeException("The media type {$content_type} is not supported");
-                    }
-                }
-                $body = DataTransferFactory::byString(file_get_contents('php://input'), $type);
             }
-            if (!empty($body)) {
-                $req = $req->withParsedBody($body)->withBody((new StreamFactory)->createStream(file_get_contents('php://input')));
+            if (!empty($input = file_get_contents('php://input'))) {
+                $req = $req->withParsedBody($body)->withBody((new StreamFactory)->createStream($input));
             }
         }
         return $req->withUri($uri);
     }
-
-    public function createServerResponse(ServerRequestInterface $server_request): ResponseInterface
-    {
-        $response = (new ResponseFactory)->createResponse()->withProtocolVersion($server_request->getProtocolVersion());
-        $accepts = $server_request->hasHeader('accept') ? $server_request->getHeaderLine('accept') : '';
-        if (!empty($accepts)) {
-            foreach (explode(',', $accepts) as $accept) {
-                if (in_array($accept, [MimeTypes::HTML, MimeTypes::JSON, MimeTypes::CSV, MimeTypes::XML, MimeTypes::EXCEL])) {
-                    $content_type = $accept;
-                    break;
-                }
-            }
-            if (empty($content_type)) {
-                throw new UnsupportedMediaTypeException("Any acepted media type ({$accepts}) are supported");
-            }
-            $response = $response->withAddedHeader('Content-type', $content_type);
-        }
-        return $response;
-    }
-
 }
