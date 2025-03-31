c43110ee75728103e64f090024d3d9880ff9d6c3
Juan Sánchez <JuanchoSL@hotmail.com>
2025-03-31 18:57:10 +0200

    interfaces documentation

--- src/Contracts/BodyCreators.php
+++ src/Contracts/BodyCreators.php
@@ -6,5 +6,11 @@ use Stringable;
 
 interface BodyCreators extends Stringable
 {
+
+    /**
+     * Append data to encode, you can add multidimensionals arrays in order to complete values when are availables
+     * @param array $data Data to append
+     * @return void
+     */
     public function appendData(array $data): static;
 }
--- src/Contracts/BodyParsers.php
+++ src/Contracts/BodyParsers.php
@@ -4,9 +4,22 @@ namespace JuanchoSL\HttpData\Contracts;
 
 interface BodyParsers
 {
+
+    /**
+     * Put into superglobals the posted values
+     * @return void
+     */
     public function toPostGlobals(): void;
 
+    /**
+     * Retrieve the posted body values and convert to array, data form are available into index 0 and files, if availables, into index 1
+     * @return array The extracted values
+     */
     public function getBodyParts(): array;
 
+    /**
+     * Retrieve the posted data form values and convert it to an array
+     * @return array The extracted data form values
+     */
     public function getBodyParams(): array;
 }
