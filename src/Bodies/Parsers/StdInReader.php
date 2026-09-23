ea4066c2baefc3e239f15bc3de565ee119c428fb
Juan Sánchez <JuanchoSL@hotmail.com>
2026-09-23 07:04:11 +0200

    undefined constant for older versions

--- src/Bodies/Parsers/StdInReader.php
+++ src/Bodies/Parsers/StdInReader.php
@@ -13,7 +13,7 @@ class StdInReader
 
     public function __construct()
     {
-        $resource = (strtolower(php_sapi_name()) == 'cli') ? STDIN : fopen('php://input', 'rb');
+        $resource = (strtolower(php_sapi_name()) == 'cli') ? fopen('php://stdin', 'rb') : fopen('php://input', 'rb');
         //foreach (['php://stdin', 'php://input'] as $input) {
             //$resource = fopen($input, "rb");
             $body = (new StreamFactory())->createStreamFromResource($resource);
